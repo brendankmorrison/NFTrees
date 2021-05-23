a8f6113d7ebf05abc8c636c400924a2986680455
Brendan Morrison <b.k.morrison2@gmail.com>
2021-05-22 21:47:35 -0700

    added landing page count up

--- NFTrees-app/src/components/Pages/Landing.js
+++ NFTrees-app/src/components/Pages/Landing.js
@@ -3,13 +3,14 @@ import './Landing.css';
 import { HashLink } from 'react-router-hash-link';
 import {BsChevronCompactDown} from 'react-icons/bs';
 import { IconContext } from "react-icons";
+import CountUp from 'react-countup';
 
 function Landing (props){
 
     return(
         <div className = 'landingContainer' id = 'landing'>
             <div className = 'landingContent'>
-            <div className = 'treeCountNumber'> 1XXXXX </div>
+            <CountUp className = 'treeCountNumber' end = {100000} duration = {4}/>
             <div className = 'treeCount'> NFTrees Planted</div>
             <div className = 'arrowContainer'>
                 <HashLink exact smooth className = "arrow my-auto" to = '/#home'>
