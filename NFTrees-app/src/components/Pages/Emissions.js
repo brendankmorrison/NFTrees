e1402134b1e4f2935d082bd289da2922e50bc0be
Brendan Morrison <b.k.morrison2@gmail.com>
2021-05-30 21:36:11 -0700

    emissions

--- NFTrees-app/src/components/Pages/Emissions.js
+++ NFTrees-app/src/components/Pages/Emissions.js
@@ -6,7 +6,7 @@ function Emissions (props){
 
     const handleCalculateEmissions = async () => {
         const address = props.address;
-        const apiKey = `4QBBENHPDFF6FAQGF8E71NFE9RNZH1MCPW`;
+        const apiKey = "4QBBENHPDFF6FAQGF8E71NFE9RNZH1MCPW";
         const emissions = await calculateAddressEmissions({
             transactionType: "eth",
             address,
