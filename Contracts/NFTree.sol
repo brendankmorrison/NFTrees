c37e9729e63ac307f0ec562aa6111529e2388360
Brendan Morrison <b.k.morrison2@gmail.com>
2021-06-14 19:58:55 -0700

    update contract

--- Contracts/NFTree.sol
+++ Contracts/NFTree.sol
@@ -12,56 +12,77 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 contract NFTree is Ownable, ERC721URIStorage{
     uint256 tokenId;
     mapping(string => IERC20) coins;
-    address donationWallet;
+    address offrampWallet;
     address tipWallet;
-    string[] memory tokenHash;
+    mapping(uint256 => string) tokenHash;
+    uint256 totalOffset;
 
     constructor() ERC721('NFTree', 'TREE')
     {
         tokenId = 1;
-        donationWallet = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+        offrampWallet = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
         tipWallet = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+        totalOffset = 0;
     }
 
-    function addToken(address _address, string memory _coin) public onlyOwner{
+    function addToken(address _address, string memory _coin) external onlyOwner{
         coins[_coin] = IERC20(_address);
     }
     
-    function setDonationWallet(address _address) public onlyOwner{
-        donationWallet = _address;
+    function setOfframpWallet(address _address) external onlyOwner{
+        offrampWallet = _address;
     }
     
-    function setTipWallet(address _address) public onlyOwner{
+    function getOfframpWallet() external view onlyOwner returns(address){
+        return(offrampWallet);
+    }
+    
+    function setTipWallet(address _address) external onlyOwner{
         tipWallet = _address;
     }
+    
+    function updateTokenHash(uint256 _level, string memory _hash) public onlyOwner{
+        require(_level >= 1 && _level <= 5, 'Invalid level');
+        
+        tokenHash[_level] = _hash;
+    }
+    
+    function getTokenHashes() external view onlyOwner returns(string[] memory){
+        
+    }
+    
+    function getTotalOffset() external view returns(uint256){
+        return(totalOffset);
+    }
 
-    function buyNFTree(uint256 _amount, uint256 _tip, string memory _coin) public {
-        require(msg.sender != address(0) && msg.sender != address(this), 'error');
-        require(coins[_coin].balanceOf(msg.sender) >= _amount + _tip, 'not enough balance');
-        require(coins[_coin].allowance(msg.sender, address(this)) >= _amount + _tip, 'not enough balance');
+    function buyNFTree(uint256 _numCredits, uint256 _amount, uint256 _tip, string memory _coin) external {
+        require(msg.sender != address(0) && msg.sender != address(this), 'Sending from zero address');
+        require(coins[_coin].balanceOf(msg.sender) >= _amount + _tip, 'Not enough balance');
+        require(_amount >= _numCredits * 10, 'Not enough value');
+        require(coins[_coin].allowance(msg.sender, address(this)) >= _amount + _tip, 'Not enough allowance');
         
         // transfer tokens
-        coins[_coin].transferFrom(msg.sender, donationWallet, _amount);
+        coins[_coin].transferFrom(msg.sender, offrampWallet, _amount);
         coins[_coin].transferFrom(msg.sender, tipWallet, _tip);
         
         _safeMint(msg.sender, tokenId);
         
-        if(_amount == 1){
-            //_setTokenURI(tokenId, tokenHash[0]); 
-        } else if (_amount == 10){
-            //_setTokenURI(tokenId, tokenHash[1]); 
-        } else if (_amount == 10){
-            //_setTokenURI(tokenId, tokenHash[2]); 
-        } else if (_amount == 10){
-            //_setTokenURI(tokenId, tokenHash[3]); 
+        if(_numCredits == 1){
+            _setTokenURI(tokenId, tokenHash[1]); 
+        } else if (_numCredits == 10){
+            _setTokenURI(tokenId, tokenHash[2]); 
+        } else if (_numCredits == 100){
+            _setTokenURI(tokenId, tokenHash[3]); 
+        } else if (_numCredits == 1000){
+            _setTokenURI(tokenId, tokenHash[4]); 
         } else {
-            //_setTokenURI(tokenId, tokenHash[4]); 
+            _setTokenURI(tokenId, tokenHash[5]); 
         }
         
         // add to total carbon offset log
+        totalOffset = totalOffset + _numCredits;
         
         tokenId = tokenId + 1; 
-        
     }
 
     function tokensOfOwner(address _owner) external view returns(uint256[] memory) {
@@ -91,17 +112,7 @@ contract NFTree is Ownable, ERC721URIStorage{
 
     }
 
-    function sendTo(address payable _payee) public onlyOwner{
-        require(_payee != address(0) && _payee != address(this), 'error');
-        //require(_amount > 0 && _amount <= address(this).balance, 'Not enough ether in contract.');
-        _payee.transfer(address(this).balance);
-    }
-    
-    function getContractBalance() public view returns(uint256){
-        return(address(this).balance);
-    }
-
-    function getNextTokenId() public view returns(uint256){
+    function getNextTokenId() external view returns(uint256){
         return(tokenId);
     }
 
